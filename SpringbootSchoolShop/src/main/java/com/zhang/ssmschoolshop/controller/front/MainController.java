bd3ae895991fdb74c7d877bdadf7353afb69156d
acodingaddict <970688889@qq.com>
2020-06-16 01:40:35 +0800

    append

--- SpringbootSchoolShop/src/main/java/com/zhang/ssmschoolshop/controller/front/MainController.java
+++ SpringbootSchoolShop/src/main/java/com/zhang/ssmschoolshop/controller/front/MainController.java
@@ -1,16 +1,34 @@
 package com.zhang.ssmschoolshop.controller.front;
 
-import com.zhang.ssmschoolshop.entity.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import com.zhang.ssmschoolshop.entity.Category;
+import com.zhang.ssmschoolshop.entity.CategoryExample;
+import com.zhang.ssmschoolshop.entity.Favorite;
+import com.zhang.ssmschoolshop.entity.FavoriteKey;
+import com.zhang.ssmschoolshop.entity.Goods;
+import com.zhang.ssmschoolshop.entity.GoodsExample;
+import com.zhang.ssmschoolshop.entity.ImagePath;
+import com.zhang.ssmschoolshop.entity.User;
 import com.zhang.ssmschoolshop.service.CateService;
 import com.zhang.ssmschoolshop.service.GoodsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;hoolshop.entity.GoodsExample;
+import com.zhang.ssmschoolshop.entity.ImagePath;
+import com.zhang.ssmschoolshop.entity.User;
+import com.zhang.ssmschoolshop.service.CateService;
+import com.zhang.ssmschoolshop.service.GoodsService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class MainController {
